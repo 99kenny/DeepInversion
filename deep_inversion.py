d06dcfea822fa017986eed601ac3e4c1f9b58133
99kenny <helloiamkenny@gmail.com>
2023-10-09 21:03:11 +0900

    fix from training

--- deep_inversion.py
+++ deep_inversion.py
@@ -164,7 +164,7 @@ class DeepInversion(object):
                 for i in train_dataset.targets:
                     if i == target:
                         idx.append(i)
-                inputs = torch.cat((inputs,torch.unsqueeze(train_dataset.__getitem__(random.choice(range(len(idx)))),0)))
+                inputs = torch.cat((inputs,torch.unsqueeze(train_dataset.__getitem__(random.choice(range(len(idx))))[0],0)))
         
         targets = targets.type(torch.LongTensor).to('cuda')
 
